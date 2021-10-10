3dbe74fc59e076182b1d185c7d327474235a7b3f
Vadim Roshchin <vadik-roshin@mail.ru>
2021-10-10 23:34:20 +0300

    Добавлено закрытие браузера после каждого теста

--- python/conftest.py
+++ python/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def config_browser():
     browser = webdriver.Chrome()
     browser.get('http://google.com')
